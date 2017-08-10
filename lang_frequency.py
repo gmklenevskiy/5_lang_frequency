67e5e9ddbe592981ea10431b33245c5709846e4a
gmklenevskiy <gmklenevskiy@gmail.com>
2017-08-10 14:30:49 +0300

    it was fixxed

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 import collections
+NUMBER_OF_FREQUENT_WORDS = 10
 
 def load_data(filepath):
     if not os.path.exists(filepath):
@@ -12,16 +13,16 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    result = re.findall(r'\w+',str(text))
-    return collections.Counter(result).most_common(10)
+    words = re.findall(r'\w+',str(text))
+    return collections.Counter(words).most_common(NUMBER_OF_FREQUENT_WORDS)
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
         filepath = sys.argv[1]
         text = load_data(filepath)
         print('Most frequent words: \n')
-        result = get_most_frequent_words(text)
-        for word in result:
+        words = get_most_frequent_words(text)
+        for word in words:
             print(word)  
     else:
         print("Please, enter the filepath")
