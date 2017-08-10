ac5ff417071f1ad50ebb858e83ac66e11bd57535
gmklenevskiy <gmklenevskiy@gmail.com>
2017-08-10 14:09:38 +0300

    prorgamme was written

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
-
+import sys
+import os
+import re
+import collections
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as text:
+        return text.read()
+        
 
 
 def get_most_frequent_words(text):
-    pass
-
+    result = re.findall(r'\w+',str(text))
+    return collections.Counter(result).most_common(10)
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv)>1:
+        filepath = sys.argv[1]
+        text = load_data(filepath)
+        print('Most frequent words: \n')
+        result = get_most_frequent_words(text)
+        for word in result:
+            print(word)  
+    else:
+        print("Please, enter the filepath")
